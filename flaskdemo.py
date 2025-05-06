5f68e222f633609e50f6c46af11fa8192e2dfe26
Lindsay Ward <lindsay.ward@jcu.edu.au>
2025-05-06 15:25:17 +1000

    Add comments and improve website

--- flaskdemo.py
+++ flaskdemo.py
@@ -2,22 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import wikipedia
 
 app = Flask(__name__)
-# Set the secret key. Keep this really secret:
+# This Flask app sets app.secret_key, which is an encryption key used "to sign cookies and other things".
+# Our app will work without it, but not completely. Without the secret key, we receive an error when searching:
+# RuntimeError: The session is unavailable because no secret key was set.
+# Storing secrets in plain code like this is not good practice. We know it.
+# We don't want you to think we're endorsing this practice!
 app.secret_key = 'IT@JCUA0Zr98j/3yXa R~XHH!jmN]LWX/,?RT'
 
 
 @app.route('/')
 def home():
+    """Home page route."""
     return render_template("home.html")
 
 
 @app.route('/about')
 def about():
+    """About page route."""
     return "I am still working on this"
 
 
 @app.route('/search', methods=['POST', 'GET'])
 def search():
+    """Search page route. Return either form page to search, or search results."""
     if request.method == 'POST':
         session['search_term'] = request.form['search']
         return redirect(url_for('results'))
@@ -26,21 +33,24 @@ def search():
 
 @app.route('/results')
 def results():
+    """Results page route. Render the search results."""
     search_term = session['search_term']
     page = get_page(search_term)
     return render_template("results.html", page=page)
 
 
 def get_page(search_term):
+    """Get a Wikipedia page object based on the search term."""
+    # This function is not a route
     try:
         page = wikipedia.page(search_term)
     except wikipedia.exceptions.PageError:
-        # no such page, return a random one
+        # No such page, so return a random one
         page = wikipedia.page(wikipedia.random())
     except wikipedia.exceptions.DisambiguationError:
-        # this is a disambiguation page, get the first real page (close enough)
+        # This is a disambiguation page; get the first real page (close enough)
         page_titles = wikipedia.search(search_term)
-        # sometimes the next page has the same name (different caps), so don't try the same again
+        # Sometimes the next page has the same name (different caps), so don't try the same again
         if page_titles[1].lower() == page_titles[0].lower():
             title = page_titles[2]
         else:
